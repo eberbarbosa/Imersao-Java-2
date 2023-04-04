d3d07456f8b41b967b26271b86c49a4521ea8ca3
eberbarbosa <eberbarbosa255@gmail.com>
2023-04-03 21:48:50 -0300

    Aula_03

--- Imersao-Java-2/src/aula_01/App.java
+++ Imersao-Java-2/src/aula_01/App.java
@@ -1,83 +1,47 @@
 
 package aula_01;
 
+import aula_02.GeradoraDeFigurinhas;
+import aula_03.API;
+import aula_03.ClienteHttp;
+import aula_03.Conteudo;
+import aula_03.ExtratorDeConteudo;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.List;
+
 /**
  *
  * @author eber
  */
-import aula_02.GeradoraDeFigurinhas;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.net.URI;
-import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
-import java.util.List;
-import java.util.Map;
-
 public class App {
 
     public static void main(String[] args) throws Exception {
 
-        // fazer uma conexão HTTP e buscar os top 250 filmes
-        // String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
-        //String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-        //String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopTVs.json";
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
-        URI endereco = URI.create(url);
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
+        API api = API.NASA;
 
-        // extrair só os dados que interessam (titulo, poster, classificação)
-        var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
-        
-         var diretorio = new File("saida/");
-        diretorio.mkdir();    
-        
+        String url = api.getUrl();
+        ExtratorDeConteudo extrator = api.getExtrator();
+
+        var http = new ClienteHttp();
+        String json = http.buscaDados(url);
 
         // exibir e manipular os dados 
+        List<Conteudo> conteudos = extrator.extraiConteudos(json);
+
         var geradora = new GeradoraDeFigurinhas();
+
         for (int i = 0; i < 3; i++) {
-            Map<String, String> filme = listaDeFilmes.get(i); 
-            
-            String urlImagem     = filme.get("image");
-            String urlImagemMaior = urlImagem.replaceFirst("(@?\\.)([0-9A-Z,_]+).jpg$","$1.jpg");
-            String titulo        = filme.get("title");              
-            double classificacao = Double.parseDouble(filme.get("imDbRating"));
-            
-            String textoFigurinha;
-            InputStream imagemEber;
-            if(classificacao >= 8.0) {
-                textoFigurinha = "TOPZERA";
-                imagemEber = new FileInputStream(new File("sobreposicao/bom.jpg"));
-                
-            } else {
-                textoFigurinha = "!! FRACO !!";
-                imagemEber = new FileInputStream(new File("sobreposicao/ruim.jpg"));
-            }
-            
-            InputStream inputStream = new URL(urlImagemMaior).openStream();
-            String nomeArquivo = "saida/" + titulo + ".png";            
-            
-            geradora.cria(inputStream, nomeArquivo, textoFigurinha, imagemEber);
-                     
-            System.out.println("\033[1m\033[42m \u001b[37mTítulo: \033[0m\033[32m" + filme.get("title"));
-            System.out.println("\033[1m\033[44m \u001b[37mURL da Imagem:\033[0m\033[36" + filme.get("image"));
-            System.out.println("\033[1m\033[45mClassificação:\033[0m\033[35m"  + filme.get("imDbRating"));
-            //double classificacao = Double.parseDouble(filme.get("imDbRating")); 
-            int estrelas = (int)classificacao;
-            for (int j = 0; j < estrelas; j++) {                
-                System.out.print( "\u001b[33m ⭐ \u001b[0m");                
-            }
-            
-            System.out.println("\n");
-            
+
+            Conteudo conteudo = conteudos.get(i);
+
+            InputStream inputStream = new URL(conteudo.urlImagem()).openStream();
+            String nomeArquivo = "saida/" + conteudo.titulo() + ".png";
+
+            geradora.cria(inputStream, nomeArquivo);
+
+            System.out.println(conteudo.titulo());
+            System.out.println();
         }
     }
 }
